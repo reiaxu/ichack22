c4a57932e5406b4c9dd72bbd39769038eb5b20d6
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2022-02-06 04:35:49 +0000

    Add BackendAPI Dockerfile

--- BackendAPI/backendAPI.py
+++ BackendAPI/backendAPI.py
@@ -139,5 +139,5 @@ def saveNutrition():
 
 
 if __name__ == "__main__":  # Run flask app in debug mode from terminal
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
 
