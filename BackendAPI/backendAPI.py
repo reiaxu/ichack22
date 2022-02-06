71522fd981f97a2318ed054fe16eaf2128a809d0
DavidMael <59983657+DavidMael@users.noreply.github.com>
2022-02-06 00:49:04 +0000

    separated diets and restricts

--- BackendAPI/backendAPI.py
+++ BackendAPI/backendAPI.py
@@ -15,15 +15,16 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 parser = reqparse.RequestParser()
 parser.add_argument("title")
 parser.add_argument("restrictions", action="append")
+parser.add_argument("diets", action="append")
 
 # wipe session
-@app.route("/diet", methods=["DELETE"])
+@app.route("/", methods=["DELETE"])
 def resetRestrictions():
     session.clear()
     return "ok", 201
 
 # receive additional dietary restrictions
-@app.route("/diet", methods=["POST"])
+@app.route("/diet/restrictions", methods=["PUT"])
 def updateRestrictions():
     args = parser.parse_args()
     newRestrictions = args["restrictions"]
@@ -38,9 +39,29 @@ def updateRestrictions():
     return {"ok": newRestrictions}, 201
 
 # return dietary restrictions
-@app.route("/diet", methods=["GET"])
+@app.route("/diet/restrictions", methods=["GET"])
 def showRestrictions():
     return {"restrictions": session.get("restrictions", [])}
+
+# receive additional diet categories
+@app.route("/diet", methods=["PUT"])
+def updateDiets():
+    args = parser.parse_args()
+    newDiets = args["diets"]
+
+    diets = session.get("diets", [])
+
+    for newDiet in newDiets:
+        diets.append(newDiet)
+
+    session["diets"] = diets
+
+    return {"ok": diets}, 201
+
+# return diets
+@app.route("/diet", methods=["GET"])
+def showDiets():
+    return {"diets": session.get("diets", [])}
 
 # receive picture metadata and pass to Aadi
 @app.route("/meta", methods=["POST"])
