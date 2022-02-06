9fea2916bde5ec64a2958149346b377f11ae2c9a
DavidMael <59983657+DavidMael@users.noreply.github.com>
2022-02-06 10:05:18 +0000

    meta works

--- BackendAPI/app.py
+++ BackendAPI/app.py
@@ -5,8 +5,13 @@ import requests, ast, json
 from flask_sqlalchemy import SQLAlchemy
 import backendFunctions as bf
 from http import HTTPStatus
+import recipes.recipes as rcp
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
 #key from Flask documentation, not secret
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -18,6 +23,7 @@ parser.add_argument("diets", action="append")
 parser.add_argument("tags", action="append")
 parser.add_argument("lat")
 parser.add_argument("lng")
+parser.add_argument("grocery list", action="append")
 
 # wipe session
 @app.route("/", methods=["DELETE"])
@@ -72,19 +78,24 @@ def showDiets():
 
 # receive picture metadata and pass to Aadi
 @app.route("/meta", methods=["PUT"])
+@cross_origin()
 def passOnMetadata():
     args = parser.parse_args()
     lat = args["lat"]
     lng = args["lng"]
+    print(lat, lng)
 
-    AadiURL = "http://172.31.26.187:5000"
+    #AadiURL = "http://172.31.26.187:5000"
+    AadiURL = "https://exif.ichack.8bitsqu.id"
+    print("pre----")
     tags = requests.get(url=AadiURL, params={"lat":lat,"lng":lng})
+    print("post---")
 
     cuisineDict = session.get("cuisine_dict", dict())
     c = json.loads(tags.text.replace("'", "\""))["cuisine"]
     cuisineDict[str(c)] = cuisineDict.get(str(c), 0) + 1
     session["cuisine_dict"] = cuisineDict
-    return Response(status=HTTPStatus.OK)
+    return Response("ok", status=HTTPStatus.OK, mimetype="text/plain")
 
 # test roulette
 @app.route("/tags", methods=["GET"])
@@ -97,7 +108,7 @@ def playRoulette():
     return Response(resp, status=HTTPStatus.OK, mimetype="application/json")
 
 
-# get a JSON array of recipe summaries to suggest
+# get a JSON array of recipe summaries to suggest, along with prices
 @app.route("/summaries", methods=["GET"])
 def promptSummaries():
     user_restrictions = session.get("restrictions", "")
@@ -105,27 +116,39 @@ def promptSummaries():
     cuisineDict = session.get("cuisine_dict", dict())
     user_cuisines = bf.cuisineRoulette(cuisineDict, 3)
     spoon_url = "http://172.31.26.187:5002"
-    spoon_params = '{"user_restrictions": {}, "user_diets": {}, "user_cuisines": {}}'.format(
-        user_restrictions, user_diets, user_cuisines
-    )
-    recipeSummaries = requests.get(spoon_url, params=spoon_params)
+
+    cuisines = ", ".join(user_cuisines)
+    if len(user_diets) > 0:
+        diet = user_diets[0]
+    else:
+        diet = ""
+    restrictions = ", ".join(user_restrictions) 
+    recipeSummaries = rcp.query_recipes(cuisines, diet, restrictions)
+
+    d = {"recipes": recipeSummaries}
+    resp = json.dumps(d)
+
     return Response(
-        response=str(recipeSummaries),
+        response=resp,
         status=HTTPStatus.OK,
         mimetype="application/json",
     )
-    # request from Reia with these paramaters
 
-# get the JSON for a specific recipe, by Id, along with specific prices
-@app.route("/recipes", methods=["GET"])
+# get the price data for a specific grocery list
+@app.route("/price", methods=["GET"])
 def promptRecipe():
     args = parser.parse_args()
-    recipe_id = args["id"]
-    spoon_url = "http://172.31.26.187:5002"
-    spoon_params = '{"id": {}}'.format(recipe_id)
-    recipe = requests.get(spoon_url, params=spoon_params)
+    grocery_list = args["grocery list"]
+
+    grocery_url = "http://146.169.190.40:4000"
+    glist = json.dumps({"name": grocery_list})
+    total = requests.get(url=grocery_url, params={glist})
+
+    d = {"total": total}
+    resp = json.dumps(d)
+
     return Response(
-        response=str(recipe),
+        response=resp,
         status=HTTPStatus.OK,
         mimetype="application/json"
     )
